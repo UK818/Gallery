1da295b1a9d92ba420cbfcce9621bb8d50092532
Usman Kulaha <32143087+UK818@users.noreply.github.com>
2022-06-04 20:25:34 +0100

    updated tabBar Controller

--- Gallery/View/View Controllers/TabBarController.swift
+++ Gallery/View/View Controllers/TabBarController.swift
@@ -12,30 +12,18 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 		setUpTabBar()
-		setTabbarOpacity()
     }
 	
 	private func setUpTabBar() {
 		viewControllers = [navigateToExplorePage(), navigateToCategoryPage(), navigateToSettingsPage()]
-		self.tabBar.unselectedItemTintColor = UIColor.white
+		self.tabBar.unselectedItemTintColor = UIColor.gray
 		tabBar.tintColor = .white
-	}
-	
-	private func setTabbarOpacity() {
-		let transperentBlackColor = UIColor(displayP3Red: 0, green: 0, blue: 0, alpha: 0.75)
-		let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-		UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
-		transperentBlackColor.setFill()
-		UIRectFill(rect)
-		
-		if let image = UIGraphicsGetImageFromCurrentImageContext() {
-			tabBar.backgroundImage = image
-		}
-		UIGraphicsEndImageContext()
+		tabBar.backgroundColor = UIColor(white: 0.3, alpha: 0.4)
 	}
 
 	private func navigateToExplorePage() -> UINavigationController {
-		let exploreViewController = ExploreViewController()
+		let viewLayout = ViewLayout()
+		let exploreViewController = ExploreViewController(viewLayout: viewLayout)
 		exploreViewController.tabBarItem.title = Constants.TabBarTitle.explore
 		exploreViewController.tabBarItem.image = Constants.Images.explore?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
 		return UINavigationController(rootViewController: exploreViewController)
@@ -43,14 +31,14 @@ class TabBarController: UITabBarController {
 	
 	private func navigateToCategoryPage() -> UINavigationController {
 		let exploreViewController = CategoryViewController()
-		exploreViewController.tabBarItem.title = Constants.TabBarTitle.explore
+		exploreViewController.tabBarItem.title = Constants.TabBarTitle.category
 		exploreViewController.tabBarItem.image = Constants.Images.category?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
 		return UINavigationController(rootViewController: exploreViewController)
 	}
 	
 	private func navigateToSettingsPage() -> UINavigationController {
 		let exploreViewController = SettingsViewController()
-		exploreViewController.tabBarItem.title = Constants.TabBarTitle.explore
+		exploreViewController.tabBarItem.title = Constants.TabBarTitle.settings
 		exploreViewController.tabBarItem.image = Constants.Images.settings?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
 		return UINavigationController(rootViewController: exploreViewController)
 	}
